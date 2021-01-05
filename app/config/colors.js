fd994093acb4c74cdac3ce3c2f6fd12c38fd2d66
nickmwangemi <nickmwangemi@gmail.com>
2021-01-05 18:30:51 +0300

    Build Separator

--- app/config/colors.js
+++ app/config/colors.js
@@ -4,5 +4,6 @@ export default {
 	black: '#000',
 	white: '#fff',
 	medium: '#6e6969',
+	light: '#f8f4f4',
 	dark: '#0c0c0c',
 }
--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { FlatList, StyleSheet } from 'react-native'
+import { FlatList, StyleSheet, View } from 'react-native'
 
 import ListItem from '../components/ListItem'
+import ListItemSeparator from '../components/ListItemSeparator'
 import Screen from '../components/Screen'
 
 const messages = [
@@ -33,6 +34,7 @@ export default function MessagesScreen() {
 						image={item.image}
 					/>
 				)}
+				ItemSeparatorComponent={() => <ListItemSeparator />}
 			></FlatList>
 		</Screen>
 	)
