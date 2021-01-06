b9c424fcd4383f0c007dc526be2de4b465a5541b
nickmwangemi <nickmwangemi@gmail.com>
2021-01-06 14:05:06 +0300

    Add logic to show Picker items

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -1,28 +1,59 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import React, { useState } from 'react'
+import {
+	StyleSheet,
+	View,
+	To,
+	TouchableWithoutFeedback,
+	Modal,
+	Button,
+	FlatList,
+} from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 import defaultStyles from '../config/styles'
 import AppText from './AppText'
+import Screen from './Screen'
+import PickerItem from './PickerItem'
 
-export default function AppPicker({ icon, placeholder, ...otherProps }) {
+export default function AppPicker({ icon, items, placeholder }) {
+	const [modalVisible, setModalVisible] = useState(false)
+
 	return (
-		<View style={styles.container}>
-			{icon && (
-				<MaterialCommunityIcons
-					name={icon}
-					size={30}
-					color={defaultStyles.colors.medium}
-					style={styles.icon}
-				/>
-			)}
-			<AppText style={styles.text}>{placeholder}</AppText>
-			<MaterialCommunityIcons
-				name='chevron-down'
-				size={30}
-				color={defaultStyles.colors.medium}
-			/>
-		</View>
+		<>
+			<TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+				<View style={styles.container}>
+					{icon && (
+						<MaterialCommunityIcons
+							name={icon}
+							size={30}
+							color={defaultStyles.colors.medium}
+							style={styles.icon}
+						/>
+					)}
+					<AppText style={styles.text}>{placeholder}</AppText>
+					<MaterialCommunityIcons
+						name='chevron-down'
+						size={30}
+						color={defaultStyles.colors.medium}
+					/>
+				</View>
+			</TouchableWithoutFeedback>
+			<Modal visible={modalVisible} animationType='slide'>
+				<Screen>
+					<Button title='Close' onPress={() => setModalVisible(false)} />
+					<FlatList
+						data={items}
+						keyExtractor={(item) => item.value.toString()}
+						renderItem={({ item }) => (
+							<PickerItem
+								label={item.label}
+								onPress={() => console.log(item)}
+							/>
+						)}
+					/>
+				</Screen>
+			</Modal>
+		</>
 	)
 }
 
